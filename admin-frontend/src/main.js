3c225175daa770855622d2a516fdd56f49e475ec
Cristian <thiess.cristi@gmail.com>
2021-12-30 17:54:42 +0200

    added store and more base project files

--- admin-frontend/src/main.js
+++ admin-frontend/src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import store from './store'
 
 axios.defaults.baseURL = 'http://localhost:3000'
 
-createApp(App).use(router, axios).mount('#app')
+createApp(App).use(store).use(router, axios).mount('#app')
